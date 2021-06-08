99d115c1c5cc440afaf9c6c23facb75ea0e46b21
Emil Pedersen <emil.pedersen@stretch.se>
2021-06-08 15:06:39 +0200

    design project update

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -98,7 +98,8 @@ export default {
       height: 250px;
 
       max-width: 600px;
-      background: #1f1f1f;
+      background-color: #2d3436;
+      background-image: linear-gradient(315deg, #2d3436 0%, #1f1f1f 74%);
       box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.07),
         0 4px 8px rgba(0, 0, 0, 0.07), 0 8px 16px rgba(0, 0, 0, 0.07),
         0 16px 32px rgba(0, 0, 0, 0.07), 0 32px 64px rgba(0, 0, 0, 0.07);
