b3386294ed8fdd04539837242657a77784ac1ab1
Emil Pedersen <emil.pedersen@refined.com>
2022-02-11 10:16:41 +0100

    adding history mode router

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,45 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '*',
+    name: 'Not found',
+    component: () => import('../views/NotFound.vue'),
   },
   {
-    path: "/projects",
-    name: "Projects",
-    component: () => import("../views/Projects.vue"),
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
   },
   {
-    path: "/resume",
-    name: "Resume",
-    component: () => import("../views/Resume.vue"),
+    path: '/projects',
+    name: 'Projects',
+    component: () => import('../views/Projects.vue'),
   },
   {
-    path: "/contact",
-    name: "Contact",
-    component: () => import("../views/Contact.vue"),
+    path: '/resume',
+    name: 'Resume',
+    component: () => import('../views/Resume.vue'),
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact.vue'),
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.path !== "/") {
-    let body = document.getElementsByTagName("body")[0];
-    body.style.overflowY = "auto";
+  if (to.path !== '/') {
+    let body = document.getElementsByTagName('body')[0];
+    body.style.overflowY = 'auto';
   }
   next();
 });
